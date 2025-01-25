426a33274908605caf7198bb4550991e477d9b84
MasonBurzy <mason.burzynski@bsu.edu>
2025-01-25 16:28:14 +0000

    modified sort.c and README.md

--- README.md
+++ README.md
@@ -1,6 +1,47 @@
-# CS121_project_2
+# CS121-project-2
+
+# CS
 
 ## Algorithm for sorting numbers
+
+## main
+
+```
+produce and array of integers
+print the array
+create two temporary integers
+test the swap function
+run the sort function on the arrays
+print the sorted array
+```
+## print values
+
+```
+void printValues(int*);
+takes an int pointer representing the array
+step through the array with a for loop
+        print each number of the array
+print a new line at the end
+return viod
+```
+
+## swap
+
+```
+void swap(int* a, int* b);
+takes two int pinter parameters
+make a temporary integer called temp
+copy the value of a to temp
+copy the value of b to the value of a
+copy temp to the value of b
+```
+
+## sort
+
+```
+
+void sort(int*);
+use bubble sort on the array
 
 constant MAX is max length of array
 function sort (array):
@@ -10,5 +51,3 @@ function sort (array):
             if array[j] > array[j+1]:
                 swap array[j] with array[j+1]
                 printArray(array)
-
-
--- sort.c
+++ sort.c
@@ -5,24 +5,58 @@ void printValues(int*);
 void sort(int*);
 void swap(int*, int*);
 
-// start main
-
 int main(){
-  int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
-  printf("Before: \n");
-  printValues(values);
+	int values [] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
+	printf("Before: \n");
+	printValues(values);
 
-  // test swap
-  int x = 3;
-  int y = 5;
-  printf("x: %d, y: %d \n", x, y);
-  swap(&x, &y);
-  printf("x: %d, y: %d \n", x, y);
+	// test swap
+	
+	int x = 3;
+	int y = 5;
+	printf("x: %d, y: %d \n", x, y);
+	swap(&x, &y);
+	printf("x: %d, y: %d \n", x, y);
+	
+	sort(values);
+	printf("After: \n");
+	printValues(values);
 
-  sort(values);
-  printf("After: \n");
-  printValues(values);
-
-  return(0);
-
+	return(0);
+	
 } // end main
+
+// start printValues
+
+void printValues(int* array){
+	for (int i = 0; i < MAX, i++;){
+		printf("%d ", array[i]);
+}
+	printf("] \n");
+
+} // end printValues
+
+
+// start sort
+
+void sort(int*);
+
+
+
+
+
+// end sort
+
+
+
+// start swap
+
+void swap(int* a, int* b){
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+} // end swap
+
+
